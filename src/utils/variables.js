9b1e07df660940e0d19bf0e51e3d8932ad0c73f0
Alex Regazzo <alex_regazzo@hotmail.com>
2025-01-26 21:10:58 -0300

    Env

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,9 +1,10 @@
 export function get_api_host() {
+  return process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:8000";
   // return "https://3l03puaz4j.execute-api.sa-east-1.amazonaws.com/dev";
   // return "http://mylocaldbalex.duckdns.org:8000";
-  return "http://localhost:8000";
 }
 export function get_socket_host() {
+  return process.env.NEXT_PUBLIC_WS_ENDPOINT || "wss://i2751iwn1e.execute-api.us-east-1.amazonaws.com/production";
   return "wss://i2751iwn1e.execute-api.us-east-1.amazonaws.com/production";
 }
 

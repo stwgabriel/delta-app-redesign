cbc722e5573092125cd3180c61df1967e167a247
Alex Regazzo <alex_regazzo@hotmail.com>
2025-03-01 18:23:38 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
               <ClientNotificationContextProvider>
                 <Header />
                 {children}
-                <Footer />
+                {/* <Footer /> */}
               </ClientNotificationContextProvider>
             </WebSocketContextProvider>
           </APIContextProvider>
